b79491b0b71f7a0e9c9071920cc2a6eaaa88d958
Alberto Fernandez <infjaf@gmail.com>
2020-11-06 14:05:25 +0100

    Test requisito 5, tierra redonda, vuelve por el otro lado

--- src/test/java/com/jcyl/remotecontrolledship/ShipTest.java
+++ src/test/java/com/jcyl/remotecontrolledship/ShipTest.java
@@ -61,4 +61,15 @@ public class ShipTest {
 		Location destino = new Location(new Point(3,2), Direction.EAST);
 		Assert.assertEquals(destino, ship.getLocation());
 	}
+	
+	@Test
+	public void saltaPorElNorte() {
+		Location loc = new Location(new Point(2, 2), Direction.NORTH);
+		Ship ship = new Ship(loc, new Planet(new Point(10,10), new ArrayList<Point>()));
+		ship.moveForward();
+		ship.moveForward();
+		Location destino = new Location(new Point(2,10), Direction.NORTH);
+		Assert.assertEquals(destino, ship.getLocation());
+	}
+	
 }
